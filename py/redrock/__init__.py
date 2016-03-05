8fabd4dec85e5eddca48d73ce33c246bdc2a5a64
Stephen Bailey <stephen.code@gmail.com>
2016-03-05 15:39:36 -0800

    post tag dev version bump

--- py/redrock/__init__.py
+++ py/redrock/__init__.py
@@ -3,7 +3,7 @@ Redrock redshift fitter
 '''
 from __future__ import absolute_import, division, print_function
 
-__version__ = '0.2'
+__version__ = '0.2.dev1'
 
 from . import rebin
 from . import zscan
