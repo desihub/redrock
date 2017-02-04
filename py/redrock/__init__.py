34261a001fdd41f93064bfdc894a4f4ce87f440f
Stephen Bailey <stephen.code@gmail.com>
2017-02-03 17:15:32 -0800

    version 0.4

--- py/redrock/__init__.py
+++ py/redrock/__init__.py
@@ -3,7 +3,7 @@ Redrock redshift fitter
 '''
 from __future__ import absolute_import, division, print_function
 
-__version__ = '0.3dev1'
+__version__ = '0.4'
 
 from .dataobj import Target, Spectrum, Template
 from . import rebin
