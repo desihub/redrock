97bdf9468e5c74b8012314927d4b3c0b33146dab
Stephen Bailey <stephen.code@gmail.com>
2017-08-14 21:16:53 -0700

    bump dev version after 0.4.2 tag

--- py/redrock/__init__.py
+++ py/redrock/__init__.py
@@ -3,7 +3,7 @@ Redrock redshift fitter
 '''
 from __future__ import absolute_import, division, print_function
 
-__version__ = '0.4.2'
+__version__ = '0.4.2.dev1'
 
 from .dataobj import Target, Spectrum, Template
 from . import rebin
