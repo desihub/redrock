249b4cd6fdd54da460a97cc1732bed031765de93
ngbusca <ngbusca@in2p3.fr>
2017-09-28 08:20:59 +0200

    removed debug output

--- py/redrock/zscan.py
+++ py/redrock/zscan.py
@@ -151,7 +151,7 @@ def calc_zchi2_targets(redshifts, targets, template, verbose=False):
     nflux = len(fluxlist[0])
     Tb = np.zeros( (nflux, nbasis) )
     for i, z in enumerate(redshifts):
-        if verbose and i in istatus or True:
+        if verbose and i in istatus:
             print('{} {}% done'.format(time.asctime(), round(100.0*i/len(redshifts))))
         #- if all targets have the same number of spectra with the same
         #- wavelength grids, we only need to calculate this once per redshift.
