a445896b6d5f47c58558bca9f157a885e73de073
Benjamin Alan Weaver <baweaver@lbl.gov>
2018-01-22 13:54:56 -0700

    remove unnecessary imports

--- py/redrock/__init__.py
+++ py/redrock/__init__.py
@@ -9,20 +9,20 @@ from __future__ import absolute_import, division, print_function
 
 from ._version import __version__
 
-from .targets import (Spectrum, Target, DistTargets)
-
-from .templates import (Template, find_templates, DistTemplate)
-
-from . import rebin
-
-from . import zscan
-
-from . import fitz
-
-from . import zwarning
-
-from . import results
-
-from .zfind import zfind
-
-from .plotspec import PlotSpec
+# from .targets import (Spectrum, Target, DistTargets)
+#
+# from .templates import (Template, find_templates, DistTemplate)
+#
+# from . import rebin
+#
+# from . import zscan
+#
+# from . import fitz
+#
+# from . import zwarning
+#
+# from . import results
+#
+# from .zfind import zfind
+#
+# from .plotspec import PlotSpec
