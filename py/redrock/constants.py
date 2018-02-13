97f88c026b11bf63772e2e995002cdfa2adffa2c
londumas <helion331990@gmail.com>
2018-02-13 10:24:46 -0800

    update with values from desisim/py/desisim/lya_mock_p1d.py

--- py/redrock/constants.py
+++ py/redrock/constants.py
@@ -7,7 +7,7 @@ Set constants used by the rest of the package.
 max_velo_diff = 1000.0  # km/s
 min_resolution_integral = 0.99
 
-### From eqn 5 of Calura et al. 2012 (Arxiv: 1201.5121)
+### From `get_tau` in `desisim/py/desisim/lya_mock_p1d.py`
 Lyman_series = {
-    'LYA' : { 'line':1215.67, 'A':0.0023, 'B':3.64 }
+    'LYA' : { 'line':1215.67, 'A':0.000318, 'B':5.10 }
 }
