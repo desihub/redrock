2df81e45f12fd01c85246621f9f7f70a05609f65
londumas <helion331990@gmail.com>
2018-02-13 11:47:49 -0800

    set Lyman-alpha to data values

--- py/redrock/constants.py
+++ py/redrock/constants.py
@@ -7,7 +7,7 @@ Set constants used by the rest of the package.
 max_velo_diff = 1000.0  # km/s
 min_resolution_integral = 0.99
 
-### From `get_tau` in `desisim/py/desisim/lya_mock_p1d.py`
+# From eqn 5 of Calura et al. 2012 (Arxiv: 1201.5121)
 Lyman_series = {
-    'LYA' : { 'line':1215.67, 'A':0.000318, 'B':5.10 }
+    'LYA' : { 'line':1215.67, 'A':0.0023, 'B':3.64 }
 }
