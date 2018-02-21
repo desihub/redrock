a8458cd3b8d019e8d48c1502f22643d150adad72
londumas <helion331990@gmail.com>
2018-02-21 10:14:03 -0800

    add other Lyman-series

--- py/redrock/constants.py
+++ py/redrock/constants.py
@@ -7,8 +7,12 @@ Set constants used by the rest of the package.
 max_velo_diff = 1000.0  # km/s
 min_resolution_integral = 0.99
 
-# From eqn 5 of Calura et al. 2012 (Arxiv: 1201.5121)
-# From McDonald et al. 2006 (Arxiv: 0405013)
+# Lyman-alpha from eqn 5 of Calura et al. 2012 (Arxiv: 1201.5121)
+# Other from eqn 1.1 of Irsic et al. 2013 , (Arxiv: 1307.3403)
 Lyman_series = {
-    'LYA' : { 'line':1215.67, 'A':0.0023, 'B':3.64, 'var_evol':3.8 }
+    'Lya'     : { 'line':1215.67,  'A':0.0023,          'B':3.64, 'var_evol':3.8 },
+    'Lyb'     : { 'line':1025.72,  'A':0.0023/5.2615,   'B':3.64, 'var_evol':3.8 },
+    'Ly3'     : { 'line':972.537,  'A':0.0023/14.356,   'B':3.64, 'var_evol':3.8 },
+    'Ly4'     : { 'line':949.7431, 'A':0.0023/29.85984, 'B':3.64, 'var_evol':3.8 },
+    'Ly5'     : { 'line':937.8035, 'A':0.0023/53.36202, 'B':3.64, 'var_evol':3.8 },
 }
