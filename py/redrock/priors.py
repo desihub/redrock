afd5286c75b65dbf239b0365fd4ac796c976ea35
londumas <helion331990@gmail.com>
2018-09-20 14:08:04 -0600

    add other type of priors

--- py/redrock/priors.py
+++ py/redrock/priors.py
@@ -4,7 +4,7 @@ Classes and functions for priors.
 
 from astropy.io import fits
 import os
-
+import numpy as np
 
 class Priors():
     """Class to store all different redshift priors.
@@ -23,16 +23,24 @@ class Priors():
         targetid = h['PRIORS'].data['TARGETID']
         z = h['PRIORS'].data['Z']
         sigma = h['PRIORS'].data['SIGMA']
-        self._priors = { targetid[i]:{'Z':z[i], 'SIGMA':sigma[i]} for i in range(z.size) }
+        self._param = { targetid[i]:{'Z':z[i], 'SIGMA':sigma[i]} for i in range(z.size) }
 
         h.close()
+
+        self._type = 'gaussian'
+        self._func = getattr(self,self._type)
 
         return
     def eval(self, targetid, z):
         try:
-            z0 = self._priors[targetid]['Z']
-            s0 = self._priors[targetid]['SIGMA']
-            return ((z-z0)/s0)**2
+            z0 = self._param[targetid]['Z']
+            s0 = self._param[targetid]['SIGMA']
+            return self._func(z,z0,s0)
         except KeyError:
             print('DEBUG: targetid {} not in priors'.format(targetid))
             return 0.
+
+    def gaussian(self,z,z0,s0):
+        return ((z-z0)/s0)**2
+    def lorentzien(self,z,z0,s0):
+        return -np.log(1.+((z-z0)/s0)**2)
