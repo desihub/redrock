2a45c4d0b2fa3f499669530a308a61b5823e5a3f
RaffaellaCapasso <rg.capasso@gmail.com>
2022-08-18 06:20:14 -0700

    added wavelength coverage check

--- py/redrock/zscan.py
+++ py/redrock/zscan.py
@@ -140,8 +140,6 @@ def calc_zchi2(target_ids, target_data, dtemplate, progress=None, use_gpu=False)
     zchi2 = np.zeros( (ntargets, nz) )
     zchi2penalty = np.zeros( (ntargets, nz) )
     zcoeff = np.zeros( (ntargets, nz, nbasis) )
-
-    # print('test3')
     
     # Redshifts near [OII] and [OIII]; used only for galaxy templates
     if dtemplate.template.template_type == 'GALAXY':
@@ -175,12 +173,12 @@ def calc_zchi2(target_ids, target_data, dtemplate, progress=None, use_gpu=False)
             zchi2[j,i], zcoeff[j,i] = calc_zchi2_one(target_data[j].spectra,
                 weights, flux, wflux, dtemplate.local.data[i])
             
-            print(dtemplate.local.data[i])
+            print(dtemplate.local.redshifts[i])
             #- Penalize chi2 for negative [OII] and [OIII] flux; ad-hoc
             if dtemplate.template.template_type == 'GALAXY':
                 
                 #check for wavelength coverage
-                if wO3cont[-1]<9825.:  #*(1+dtemplate.local.data[i])?
+                if wO3line[-1]*(1+dtemplate.local.redshifts[i]) < 9825.:
 
                     fO2line = np.sum(O2line.dot(zcoeff[j,i]))
                     fO2range = np.sum(O2range.dot(zcoeff[j,i]))
