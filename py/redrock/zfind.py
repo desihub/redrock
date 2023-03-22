d6759f3e5d0bb7bf283b9cd3733920fdb38b3a4e
Abhijeet Anand <abhijeetanand2011@gmail.com>
2023-03-22 15:53:07 -0700

    resolved array reshape in fulltype

--- py/redrock/zfind.py
+++ py/redrock/zfind.py
@@ -410,17 +410,20 @@ def zfind(targets, templates, mp_procs=1, nminima=3, archetypes=None, priors=Non
                         spectype, subtype = fulltype.split(':::')
                     else:
                         spectype, subtype = (fulltype, '')
+                    nmin = len(tmp['chi2']) # defining dimension of array to save fulltype
                 else:
-                    spectype = [ el.split(':::')[0] for el in tmp['fulltype'] ]
-                    subtype = [ el.split(':::')[1] for el in tmp['fulltype'] ]
-                    tmp.remove_column('fulltype')
+                    spectype = [ el[0].split(':::')[0] for el in tmp['fulltype'] ] #el is a list with one element (corresponding to each minima)
+                    subtype = [ el[0].split(':::')[1] for el in tmp['fulltype'] ]
+                    del tmp['fulltype'] #it's a dictionary
+                    nmin=1 # defining dimension of array to save fulltype (spectype and subtype are already a list with len(tmp['chi2']) elements
+                    #tmp.remove_column('fulltype')
 
                 #Have to create arrays of correct length since using dict of
                 #np arrays instead of astropy Table
                 l = len(tmp['chi2'])
-                tmp['spectype'] = np.array([spectype]*l).reshape((l, 1))
-                tmp['subtype'] = np.array([subtype]*l).reshape((l, 1))
-
+                tmp['spectype'] = np.array([spectype]*nmin).reshape((l, 1))
+                tmp['subtype'] = np.array([subtype]*nmin).reshape((l, 1))
+                
                 tmp['ncoeff'] = np.array([tmp['coeff'].shape[1]]*l).reshape((l, 1))
                 tzfit.append(tmp)
                 del allresults[tid][fulltype]['zfit']
