580a6b62ec19720bf1865b32146fa618aa208e0c
a_anand <abhijeetanand2011@gmail.com>
2023-03-22 00:14:06 -0700

    debugger added

--- py/redrock/archetypes.py
+++ py/redrock/archetypes.py
@@ -125,7 +125,7 @@ class Archetype():
             ### Applying nearest neighbour method based on artifical galaxies with known physical properties
             zzchi2 = np.zeros(n_nbh, dtype=np.float64)
             zzcoeff = np.zeros((n_nbh, nleg+1), dtype=np.float64)
-            subtype = self._full_type[iBest].split('_')[0] #redrock best subtype
+            subtype = self._full_type[iBest].split(':::')[0] #redrock best subtype
             if subtype in ['ELG', 'LRG', 'BGS']:
                 new_arch, gal_inds = return_N_nearest_archetypes_from_synthetic_spectra(arch_id=iBest, archetype_data=rrarchs_params, gal_data=archetype_galaxies[subtype], n_nbh=n_nbh, ret_wave=False)
                 for i in range(n_nbh):
@@ -152,6 +152,7 @@ class Archetype():
             tdata = { hs:np.append(binned[hs][:,None],legendre[hs].transpose(), axis=1 ) for hs, wave in dwave.items() }
             zzchi2, zzcoeff = calc_zchi2_one(spectra, weights, flux, wflux, tdata)
             chi2, coeff, fulltype =  zzchi2, zzcoeff, self._full_type[iBest]
+        import pdb;pdb.set_trace()
         return chi2, coeff, fulltype
 
 
