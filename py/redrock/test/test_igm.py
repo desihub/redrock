4bcca08db27035356344e02c8af90cdf2b2da20b
Stephen Bailey <StephenBailey@lbl.gov>
2024-01-11 18:00:27 -0800

    cupy scalar vs. vector

--- py/redrock/test/test_igm.py
+++ py/redrock/test/test_igm.py
@@ -7,6 +7,7 @@ try:
     d = cp.cuda.Device()
     cp_available = cp.is_available() 
 except Exception:
+    cp = None
     cp_available = False
 
 from .. import igm
@@ -20,6 +21,13 @@ class TestUtils(unittest.TestCase):
     def setUpClass(cls):
         cls.obswave = np.arange(3600, 9801, 50)
         cls.redshifts = np.arange(0, 5.1, 0.5)
+
+        cls.nwave = len(cls.obswave)
+        cls.nz = len(cls.redshifts)
+
+        if cp_available:
+            cls.gpu_obswave = cp.asarray(cls.obswave)
+            cls.gpu_redshifts = cp.asarray(cls.redshifts)
 
     def test_transmission_Lyman_scalars(self, use_gpu=False):
         # scalar inputs -> scalar output
@@ -43,46 +51,63 @@ class TestUtils(unittest.TestCase):
 
 
     def test_transmission_Lyman_scalar_redshift(self, use_gpu=False):
+        if use_gpu:
+            obswave = self.gpu_obswave
+        else:
+            obswave = self.obswave
+
         for model in igm.igm_models:
             for z in self.redshifts:
                 print(f'{model=} {z=}')
-                t = igm.transmission_Lyman(z, self.obswave, model=model,
+                t = igm.transmission_Lyman(z, obswave, model=model,
                                            use_gpu=use_gpu, always_return_array=False)                
                 if self.obswave[0]/(1+z) > LyA_wavelength or model == 'None':
                     self.assertEqual(t, None)
                 else:
-                    self.assertEqual(t.shape, (len(self.obswave),) )
+                    self.assertEqual(t.shape, (self.nwave,) )
                     self.assertTrue(np.any(t < 1.0))
 
-                t = igm.transmission_Lyman(z, self.obswave, model=model,
+                t = igm.transmission_Lyman(z, obswave, model=model,
                                            use_gpu=use_gpu, always_return_array=True)
-                self.assertEqual(t.shape, (len(self.obswave),) )
+                self.assertEqual(t.shape, (self.nwave,) )
                 if self.obswave[0]/(1+z) > LyA_wavelength or model == 'None':
                     self.assertTrue(np.all(t == 1.0))
                 else:
                     self.assertTrue(np.any(t < 1.0))
 
     def test_transmission_Lyman_scalar_wavelength(self, use_gpu=False):
+        if use_gpu:
+            redshifts = self.redshifts
+        else:
+            redshifts = self.redshifts
+
         for model in igm.igm_models:
             for w in self.obswave:
                 print(f'{model=} {w=}')
-                t = igm.transmission_Lyman(self.redshifts, w, model=model,
+                t = igm.transmission_Lyman(redshifts, w, model=model,
                                            use_gpu=use_gpu, always_return_array=False)
                 if w/(1+self.redshifts[-1]) > LyA_wavelength or model == 'None':
                     self.assertEqual(t, None)
                 else:
-                    self.assertEqual(t.shape, (len(self.redshifts),) )
+                    self.assertEqual(t.shape, (self.nz,) )
                     self.assertTrue(np.any(t < 1.0))
 
-                t = igm.transmission_Lyman(self.redshifts, w, model=model,
+                t = igm.transmission_Lyman(redshifts, w, model=model,
                                            use_gpu=use_gpu, always_return_array=True)
-                self.assertEqual(t.shape, (len(self.redshifts),) )
+                self.assertEqual(t.shape, (self.nz,) )
                 if w/(1+self.redshifts[-1]) > LyA_wavelength or model == 'None':
                     self.assertTrue(np.all(t == 1.0))
                 else:
                     self.assertTrue(np.any(t < 1.0))
 
     def test_transmission_Lyman(self, use_gpu=False):
+        if use_gpu:
+            obswave = self.gpu_obswave
+            redshifts = self.redshifts
+        else:
+            obswave = self.obswave
+            redshifts = self.redshifts
+
         restwave = np.outer(1/(1+self.redshifts), self.obswave)
         for model in igm.igm_models:
             print(f'{model=}')
@@ -91,7 +116,7 @@ class TestUtils(unittest.TestCase):
             if model == 'None':
                 self.assertEqual(t, None)
             else:
-                self.assertEqual(t.shape, (len(self.redshifts), len(self.obswave)) )
+                self.assertEqual(t.shape, (self.nz, self.nwave) )
                 self.assertTrue(np.any(t < 1.0))
 
                 ii = restwave > LyA_wavelength
@@ -102,7 +127,7 @@ class TestUtils(unittest.TestCase):
             if model == 'None':
                 self.assertTrue(np.all(t == 1.0))
             else:
-                self.assertEqual(t.shape, (len(self.redshifts), len(self.obswave)) )
+                self.assertEqual(t.shape, (self.nz, self.nwave) )
                 self.assertTrue(np.any(t < 1.0))
 
                 ii = restwave > LyA_wavelength
