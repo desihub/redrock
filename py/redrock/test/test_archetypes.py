c6f61ed6ba1df9ed55024afa547cb6b0dac94ffb
Stephen Bailey <StephenBailey@lbl.gov>
2024-04-19 12:03:25 -0700

    fix archetype tests

--- py/redrock/test/test_archetypes.py
+++ py/redrock/test/test_archetypes.py
@@ -31,12 +31,12 @@ class TestArchetypes(unittest.TestCase):
         self.assertEqual(lc, [[2,3], [4,5], [6,7]])
 
         #- 2 archetypes, 3 bands, 2 legendre per band, with trailing zeros
-        ac, lc = split_archetype_coeff('ELG_12,LRG_5', [1,0,2,3,4,5,6,7,0,0,0], nbands=3)
+        ac, lc = split_archetype_coeff('ELG_12;LRG_5', [1,0,2,3,4,5,6,7,0,0,0], nbands=3)
         self.assertEqual(ac, [1,0])
         self.assertEqual(lc, [[2,3], [4,5], [6,7]])
 
         #- no legendre terms
-        ac, lc = split_archetype_coeff('ELG_12,LRG_5', [1,0,], nbands=3)
+        ac, lc = split_archetype_coeff('ELG_12;LRG_5', [1,0,], nbands=3)
         self.assertEqual(ac, [1,0])
         self.assertEqual(lc, [list(), list(), list()])
 
