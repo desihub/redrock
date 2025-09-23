a2813bc9654cca2a4dedbddf8155742557ddaa84
Abhijeet Anand <abhijeetanand2011@gmail.com>
2025-09-23 13:54:50 -0700

    test_archetype updated

--- py/redrock/test/test_archetypes.py
+++ py/redrock/test/test_archetypes.py
@@ -35,27 +35,27 @@ class TestArchetypes(unittest.TestCase):
         # followed by nbands * nleg Legendre coefficients
 
         #- 1 archetype, 3 bands, 1 legendre per band
-        ac, lc = split_archetype_coeff('ELG_12', [1,2,3,4], nbands=3)
+        ac, lc = split_archetype_coeff('ELG_12', [1,2,3,4], nbands=3, nleg=1)
         self.assertEqual(ac, [1,])
         self.assertEqual(lc, [[2,], [3,], [4,]])
 
         #- 1 archetype, 3 bands, 2 legendre per band
-        ac, lc = split_archetype_coeff('ELG_12', [1,2,3,4,5,6,7], nbands=3)
+        ac, lc = split_archetype_coeff('ELG_12', [1,2,3,4,5,6,7], nbands=3, nleg=2)
         self.assertEqual(ac, [1,])
         self.assertEqual(lc, [[2,3], [4,5], [6,7]])
 
         #- 1 archetype, 3 bands, 2 legendre per band, with trailing zeros
-        ac, lc = split_archetype_coeff('ELG_12', [1,2,3,4,5,6,7,0,0,0], nbands=3)
+        ac, lc = split_archetype_coeff('ELG_12', [1,2,3,4,5,6,7,0,0,0], nbands=3, nleg=2)
         self.assertEqual(ac, [1,])
         self.assertEqual(lc, [[2,3], [4,5], [6,7]])
 
         #- 2 archetypes, 3 bands, 2 legendre per band, with trailing zeros
-        ac, lc = split_archetype_coeff('ELG_12;LRG_5', [1,0,2,3,4,5,6,7,0,0,0], nbands=3)
+        ac, lc = split_archetype_coeff('ELG_12;LRG_5', [1,0,2,3,4,5,6,7,0,0,0], nbands=3, nleg=2)
         self.assertEqual(ac, [1,0])
         self.assertEqual(lc, [[2,3], [4,5], [6,7]])
 
         #- no legendre terms
-        ac, lc = split_archetype_coeff('ELG_12;LRG_5', [1,0,], nbands=3)
+        ac, lc = split_archetype_coeff('ELG_12;LRG_5', [1,0,], nbands=3, nleg=0)
         self.assertEqual(ac, [1,0])
         self.assertEqual(lc, [list(), list(), list()])
 
